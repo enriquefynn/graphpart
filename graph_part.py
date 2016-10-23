045dd1f453456f077cea490cf26586a1e8ec931d
Fynn <enriquefynn@gmail.com>
2016-10-23 21:21:46 +0200

    bla

--- graph_part.py
+++ graph_part.py
@@ -5,25 +5,38 @@ import tempfile, getopt, sys, os
 import numpy as np
 import scipy.sparse as sp
 
-def get_metis_instancen(partitions, n, m, seed):
-    graph = nx.Graph(nx.barabasi_albert_graph(n, m, seed = seed))
+def get_clustered_graph(partitions, vertexes, ncon):
+    graph = nx.Graph()
+    graph.add_nodes_from([0, vertexes])
+
+    m = 0
+    for i in range(partitions):
+        tmp = nx.Graph(nx.barabasi_albert_graph(vertexes, 2, seed = 0))
+        for e in tmp.edges():
+            graph.add_edge(e[0] + m*vertexes, e[1] + m*vertexes)
+        m+=1
+    return graph
+
+def get_metis_instance(partitions, n, m, seed):
+    #graph = nx.Graph(nx.barabasi_albert_graph(n, m, seed = seed))
+    graph = get_clustered_graph(partitions, n, 0)
     tmp_filename = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
     with open(tmp_filename, 'w') as tmp_graph:
         tmp_graph.write('{} {}\n'.format(graph.number_of_nodes(), graph.number_of_edges()))
-        for node in range(n):
+        for node in range(graph.number_of_nodes()):
             for edg in graph[node]:
                 tmp_graph.write('{} '.format(edg + 1))
             tmp_graph.write('\n')
         
-    os.system('{} {} {} &> /dev/null'.format('gpmetis', tmp_graph.name, partitions))
+    os.system('{} {} {}'.format('gpmetis', tmp_graph.name, partitions))
     partition_filename = '{}.part.{}'.format(tmp_filename, partitions)
     partition_data = []
     with open(partition_filename, 'r') as p:
         for l in p:
             partition_data.append(int(l))
 
-    os.system('{} {}'.format('rm', tmp_filename))
-    os.system('{} {}'.format('rm', partition_filename))
+    #os.system('{} {}'.format('rm', tmp_filename))
+    #os.system('{} {}'.format('rm', partition_filename))
     fr = []
     to = [] 
     vals = []
@@ -42,14 +55,13 @@ def get_metis_instancen(partitions, n, m, seed):
 
 
 
-
 class Network:
-    def __init__(self, layer_sizes):
+    def __init__(self, layer_sizes, std = 1.0):
         self.layer_sizes = layer_sizes
 
         self.weights = []
         for i in range(0, len(self.layer_sizes) - 1):
-            self.weights.append(np.random.normal(0.0, .01, (layer_sizes[i+1], layer_sizes[i])))
+            self.weights.append(np.random.normal(0.0, std, (layer_sizes[i+1], layer_sizes[i])))
         self.buffers = [None] * len(self.layer_sizes)
             
     def forward(self, A, x):
@@ -59,10 +71,12 @@ class Network:
 
         for i in range(len(self.weights)):
             xx = self.buffers[i]
+            
             step = np.dot(self.weights[i], xx)
             xxh = A.dot(step.T).T
-            
+                        
             if i < len(self.weights) - 1:
+                #self.buffers[i+1] = xxh
                 self.buffers[i+1] = np.tanh(xxh)
             else:
                 self.buffers[i+1] = np.exp(xxh) / np.sum(np.exp(xxh), axis=0, keepdims=True)
@@ -73,8 +87,8 @@ class Network:
         d = delta.copy()
         for i in range(len(self.weights) - 1, -1, -1):
             if i < len(self.weights) - 1:
-                before_f = d * (1.0 - self.buffers[i+1] ** 2)
-
+                d = d * (1.0 - self.buffers[i+1] ** 2)
+            
             d = A.dot(d.T).T
             dw = np.dot(self.buffers[i], d.T).T
             self.weights[i] += dw * lr  ##learning
@@ -89,8 +103,8 @@ if __name__ == '__main__':
         sys.exit(1)
     n_parts = 2
     seed = 0
-    n = 20
-    m = 3
+    n = 10
+    m = 6
 
     for opt, arg in opts:
         if opt == '-s':
@@ -100,7 +114,7 @@ if __name__ == '__main__':
         if opt == '-m':
             m = int(arg)
 
-    net = Network([n_parts, 10, 10, n_parts])
+    net = Network([n_parts, 10, 10, n_parts], std=.5)
 
 
     n_epoch = 10000
@@ -109,23 +123,30 @@ if __name__ == '__main__':
         #print "epoch: ", epoch
         #Create Random Graph with Partitions
         seed = np.random.rand()
-        A, part = get_metis_instancen(n_parts, n, m, seed = seed)
+        A, part = get_metis_instance(n_parts, n, m, seed = seed)
 
         n_times = 1000
         y = None
+        n_nodes = A.get_shape()[0] 
+        
+        #x out of loop
+        x = np.random.uniform(0, 1, (n_parts, n_nodes))
+        x = x / np.sum(x, axis=0, keepdims=True)
         for i in range(n_times):
             #Create Input
-            x = np.random.uniform(1.0 / n_parts, 0., size=(n_parts, n))
-            x = x / np.sum(x, axis=0, keepdims=True)
             
+
             #Run Network
             #y = net.forward(A, x)
             y = net.forward(A, x)
+            
             #y = net.forward(A, y)
                 
 
-            #Find optimal assignment
+            #Current Guess
             guess = np.argmax(y, axis=0)
+
+            #Find optimal assignment
             match = np.zeros((n_parts, n_parts))
             for p, g in zip(part, guess):
                 match[p, g] += 1
@@ -140,8 +161,8 @@ if __name__ == '__main__':
             mapped_part = [mapping[v] for v in part]
             
             #Create Targets
-            t = np.zeros((n_parts, n))
-            for i in range(n):
+            t = np.zeros((n_parts, n_nodes))
+            for i in range(n_nodes):
                 t[mapped_part[i], i] = 1
 
             #Run Backward pass (Learn)
@@ -150,8 +171,9 @@ if __name__ == '__main__':
             print "Y"
             print y
             delta = t - y
-            net.backward(A, delta, .001)
-                        
+            net.backward(A, delta, lr = .0003)
+            x = y
+           
         if epoch%100 == 0:
             print y
             print t
